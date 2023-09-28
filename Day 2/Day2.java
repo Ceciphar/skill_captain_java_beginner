6f4b0764627f59f702d7c0e05494d93529eb61e5
JN <23392252+Ceciphar@users.noreply.github.com>
2023-09-28 15:20:39 -0400

    day2.java added

---
+++ Day 2/Day2.java
@@ -0,0 +1,10 @@
+public class Day2 {
+    public static void main(String[] args) {
+        String userName = "Joseph";
+        int userAge = 20;
+        boolean isStudent = true;
+
+        System.out.printf("Hello my name is %s and I am %d years old. I fact that I am a student remains %b.", userName, userAge, isStudent);
+
+    }
+}
